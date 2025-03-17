f7807778b39e44b0a1bb196f0cb489ae3e7c822e
Romy-OR <131908@epsstudents.org>
2025-03-17 14:32:32 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,18 @@
 
-
 class Main {
   public static void main( String args[])
   {
-    Picture pix = new Picture("Unit 8 Project Starter Code\\images\\water.jpg");
+    Picture pix = new Picture("Unit 8 Project Starter Code\\images\\beach.jpg");
     //pix.negate();
     //pix.greyscale();
     //pix.fixUnderWater();
     //pix.zeroBlue();
     //pix.keepOnlyBlue();
-    pix.mirrorVerticalRightToLeft();
+    //pix.mirrorVerticalRightToLeft(300);
+    //pix.mirrorHorizontalBotToTop(300);
+    //pix.mirrorArms();
+    //pix.mirrorGull();
+    pix.mirrorDiagonal();
     pix.explore();
   }
 }
