75102ee4a3d00f436833c1e34619ee0ddbcf27fd
Romy-OR <131908@epsstudents.org>
2025-03-17 13:26:15 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+
+class Main {
+  public static void main( String args[])
+  {
+    Picture pix = new Picture("Unit 8 Project Starter Code\\images\\water.jpg");
+    //pix.negate();
+    //pix.greyscale();
+    //pix.fixUnderWater();
+    //pix.zeroBlue();
+    //pix.keepOnlyBlue();
+    pix.mirrorVerticalRightToLeft();
+    pix.explore();
+  }
+}
